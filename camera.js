f1f2081c8d429e7674b3c58870fd3251e4ac80bd
Toxel <84172065+ToxelEr@users.noreply.github.com>
2022-08-25 22:46:24 +0430

    Add files via upload

---
+++ camera.js
@@ -0,0 +1,24 @@
+//camera
+let [Yaw,Pitch,XAcc,YAcc] = [-155,-25,.05,-.02];
+let [YawV,PitchV,XOld,YOld] = [0,0,0,0];
+let start = true;
+
+const el = document.body;
+el.addEventListener('touchstart', touchStart);
+el.addEventListener('touchmove',touchMove);
+
+function touchStart() {
+  var XOld = event.touches[0].clientX;
+  var YOld = event.touches[0].clientY;
+  start = true;
+}
+
+function touchMove() {
+  YawV = ((event.touches[0].clientX - XOld) * XAcc) + YawV/4;
+  PitchV = ((event.touches[0].clientY - YOld) * YAcc) + PitchV/4;
+  if (start == true) {
+    XOld = event.touches[0].clientX;
+    YOld = event.touches[0].clientY;
+    start = false;
+  }
+}
---
+++ vox.js
@@ -0,0 +1,48 @@
+var canvas = document.querySelector('#canvas');
+var ctx = canvas.getContext('2d');
+
+function sprite(spr) {
+  img = new Image();
+  img.src = 'sprites/' + spr + '.png';
+  document.body.appendChild(img);
+  img.style.display = 'none';
+  img.classList.add('sprite');
+}
+
+function cls(x) {
+   ctx.clearRect(0, 0, canvas.width, canvas.height);
+   canvas.style.background = x;
+}
+
+function rotation(x1,y1,x,y,angle) {
+  a = angle
+  var radians = (Math.PI / 180) * a;
+  cos = Math.cos(radians);
+  sin = Math.sin(radians);
+  nx = (cos * (x - x1)) + (sin * (y - y1)) + x1;
+  ny = (cos * (y - y1)) - (sin * (x - x1)) + y1;
+  return [nx, ny];
+}
+
+function ssprr(x,y,w,h,x1,y1,w1,h1,r,angle) {
+    ctx.save();
+  img = document.querySelector('.sprite');
+  ctx.imageSmoothingEnabled = false;
+  ctx.translate(x+w/2,y+h/2);
+  ctx.scale(1,Math.sin(angle*(Math.PI/180)))
+  ctx.rotate(r * (Math.PI/180));
+  ctx.translate(-x-w/2,-y-h/2);
+  ctx.drawImage(img,x1,y1,w1,h1,x,y,w,h);
+  ctx.restore();
+}
+
+function ssprr2(x,y,w,h,x1,y1,w1,h1,r) {
+  ctx.save();
+  img = document.querySelector('.sprite');
+  ctx.imageSmoothingEnabled = false;
+  ctx.translate(x+w/2,y+h/2);
+  ctx.rotate(r * (Math.PI/180));
+  ctx.translate(-x-w/2,-y-h/2);
+  ctx.drawImage(img,x1,y1,w1,h1,x,y,w,h);
+  ctx.restore();
+}
