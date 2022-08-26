70ee7ebb553a1648500f6e8eff1c1ff03e6ea7b2
Toxel <84172065+ToxelEr@users.noreply.github.com>
2022-08-26 11:31:09 +0430

    Update camera.js

--- camera.js
+++ camera.js
@@ -6,12 +6,18 @@ let start = true;
 const el = document.body;
 el.addEventListener('touchstart', touchStart);
 el.addEventListener('touchmove',touchMove);
-el.addEventListener('mousedown', touchStart);
-el.addEventListener('mousemove',touchMove);
+el.addEventListener('mousedown', mouseStart);
+el.addEventListener('mousemove',mouseMove);
 
 function touchStart() {
   var XOld = event.touches[0].clientX;
   var YOld = event.touches[0].clientY;
+  start = true;
+}
+
+function mouseStart() {
+  var XOld = event.e.clientX;
+  var YOld = event.e.clientY;
   start = true;
 }
 
@@ -21,6 +27,17 @@ function touchMove() {
   if (start == true) {
     XOld = event.touches[0].clientX;
     YOld = event.touches[0].clientY;
+    start = false;
+  }
+}
+
+
+function mouseMove() {
+  YawV = ((event.clientX - XOld) * XAcc) + YawV/4;
+  PitchV = ((event.clientY - YOld) * YAcc) + PitchV/4;
+  if (start == true) {
+    XOld = event.clientX;
+    YOld = event.clientY;
     start = false;
   }
 }
